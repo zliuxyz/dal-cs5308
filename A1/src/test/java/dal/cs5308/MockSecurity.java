ea82c4cd8aeec379cd7f73a1901713bb2a1031cb
Zongming Liu <zongmingxliu@gmail.com>
2018-05-31 01:02:20 -0300

    change the comment for method

--- A1/src/test/java/dal/cs5308/MockSecurity.java
+++ A1/src/test/java/dal/cs5308/MockSecurity.java
@@ -2,8 +2,10 @@ package dal.cs5308;
 
 public class MockSecurity implements ISecurity {
 
-    // Only returns false dealerId equals to "FAKEID" and dealerAccessKey equals to "FAKEKEY", returns
-    // true otherwise.
+    /*
+    Only returns false when dealerId equals to "FAKEID" and dealerAccessKey equals to "FAKEKEY",
+    returns true otherwise.
+     */
     @Override
     public boolean isDealerAuthorized(String dealerId, String dealerAccessKey) {
         return !dealerId.equals("FAKEID") && !dealerAccessKey.equals("FAKEKEY");
