daf9c4f2e4184aac477df20ae12bf55b6ea962d5
Zongming Liu <zongmingxliu@gmail.com>
2018-06-02 18:09:03 -0300

    ignore generated OrderResponse.xml

--- A1/src/main/java/dal/cs5308/DeliveryAddress.java
+++
@@ -1,9 +0,0 @@
-package dal.cs5308;
-
-public class DeliveryAddress {
-    public String name;
-    public String street;
-    public String city;
-    public String province;
-    public String postalCode;
-}
